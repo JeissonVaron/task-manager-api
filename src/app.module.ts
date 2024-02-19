784e66e1f6963cbf31fc203a08dcd4a3041bb82a
Jeisson Rodriguez <jrodriguez.sofka.ext@sistecredito.com>
2024-02-18 21:07:29 -0500

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     TasksModule,
     MongooseModule.forRoot('mongodb://localhost:27017/task-manager'),
-    CommonModule
+    CommonModule,
+    SeedModule
   ],
   controllers: []
 })
